40328e90ed890efc8e4192be7b7e63c4af85bd7c
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2024-11-04 00:49:56 +0600

    add open api config

---
+++ src/main/java/global/inventory/config/OpenAPIConfiguration.java
@@ -0,0 +1,18 @@
+package global.inventory.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfiguration {
+}
