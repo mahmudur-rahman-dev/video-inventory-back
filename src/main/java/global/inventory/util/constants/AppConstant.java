aac90a6a8f899bbb73dbab4d206b0cfc4f9a4922
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2024-11-07 22:40:57 +0600

    clean up

--- src/main/java/global/inventory/util/constants/AppConstant.java
+++ src/main/java/global/inventory/util/constants/AppConstant.java
@@ -2,8 +2,4 @@ package global.inventory.util.constants;
 
 public class AppConstant {
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String[] API_ENDPOINTS_WHITELIST = {
-            "/api/v1/auth/login",
-            "/api/v1/auth/refresh-token"
-    };
 }
